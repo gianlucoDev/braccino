da65662df539b01e97c8f2d64fd05fde6e857be9
darcros <darcros7@gmail.com>
2021-04-23 21:30:16 +0200

    add CORS headers

--- braccino/settings.py
+++ braccino/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'braccio',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Allow CORS requests from all domains
+CORS_ALLOW_ALL_ORIGINS = True
