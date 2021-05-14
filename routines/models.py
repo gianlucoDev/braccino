f865ffcdc5a52edbde4ad0dcf3b081776a26f556
darcros <darcros7@gmail.com>
2021-05-14 17:41:47 +0200

    made indivdual joints private

--- routines/models.py
+++ routines/models.py
@@ -25,32 +25,32 @@ class Step(models.Model):
     speed = models.IntegerField()
 
     # position
-    m1 = models.IntegerField()
-    m2 = models.IntegerField()
-    m3 = models.IntegerField()
-    m4 = models.IntegerField()
-    m5 = models.IntegerField()
-    m6 = models.IntegerField()
+    _m1 = models.IntegerField()
+    _m2 = models.IntegerField()
+    _m3 = models.IntegerField()
+    _m4 = models.IntegerField()
+    _m5 = models.IntegerField()
+    _m6 = models.IntegerField()
 
     @property
     def position(self) -> Position:
         return Position(
-            base=self.m1,
-            shoulder=self.m2,
-            elbow=self.m3,
-            wrist_ver=self.m4,
-            wrist_rot=self.m5,
-            gripper=self.m6,
+            base=self._m1,
+            shoulder=self._m2,
+            elbow=self._m3,
+            wrist_ver=self._m4,
+            wrist_rot=self._m5,
+            gripper=self._m6,
         )
 
     @position.setter
     def position(self, position: Position):
-        self.m1 = position.base
-        self.m2 = position.shoulder
-        self.m3 = position.elbow
-        self.m4 = position.wrist_ver
-        self.m5 = position.wrist_rot
-        self.m6 = position.gripper
+        self._m1 = position.base
+        self._m2 = position.shoulder
+        self._m3 = position.elbow
+        self._m4 = position.wrist_ver
+        self._m5 = position.wrist_rot
+        self._m6 = position.gripper
 
     class Meta:
         ordering = ['order']
